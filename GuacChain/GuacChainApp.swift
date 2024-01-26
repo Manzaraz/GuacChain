efee87eedf1dfc3ef05012266d59e6a795d859b8
Christian Manzaraz <magnusmanz@gmail.com>
2024-01-26 00:06:39 -0300

    Initial Commit

---
+++ GuacChain/GuacChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuacChainApp.swift
+//  GuacChain
+//
+//  Created by Christian Manzaraz on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuacChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
